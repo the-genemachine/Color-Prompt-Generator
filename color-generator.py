150b6149bf7ae00c0123093e9e7100949ddf81b7
the-genemachine <gene.gallant@rogers.com>
2023-06-04 23:46:06 -0400

    Testing the response from the API via python file : Working OK

--- color-generator.py
+++ color-generator.py
@@ -1,8 +1,15 @@
 import openai
 
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
 openai.api_key = config["OPENAI_API_KEY"]
 
+response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt="Give me a tagline for a color prompt generator",
+    max_tokens=50
+)
+
+print(response)
